d5af8b63258be87848147fe6dc2c4fec95ec8d8e
RobotMan2412 <julian@scheffers.net>
2022-05-28 15:44:03 +0200

    Finish basic scanning routines.

--- main/include/wifi_connection.h
+++ main/include/wifi_connection.h
@@ -5,7 +5,32 @@
 
 #include "esp_wifi.h"
 
+// Simpler interpretation of WiFi signal strength.
+typedef enum {
+	WIFI_STRENGTH_VERY_BAD,
+	WIFI_STRENGTH_BAD,
+	WIFI_STRENGTH_GOOD,
+	WIFI_STRENGTH_VERY_GOOD,
+} wifi_strength_t;
+
+// Thresholds for aforementioned signal strength definitions.
+#define WIFI_THRESH_BAD       -80
+#define WIFI_THRESH_GOOD      -70
+#define WIFI_THRESH_VERY_GOOD -67
+
+// Firt time initialisation of the WiFi stack.
 void wifi_init();
+
+// Connect to a traditional username/password WiFi network.
 bool wifi_connect(const char* aSsid, const char* aPassword, wifi_auth_mode_t aAuthmode, uint8_t aRetryMax);
+
+// Connect to a WPA2 enterprise WiFi network.
 bool wifi_connect_ent(const char* aSsid, const char *aIdent, const char *aAnonIdent, const char* aPassword, uint8_t aRetryMax);
-void wifi_scan();
+
+// Scan for WiFi networks.
+// Updates the APs pointer if non-null.
+// Returns the number of APs found.
+size_t wifi_scan(wifi_ap_record_t **aps);
+
+// Get the strength value for a given RSSI.
+wifi_strength_t wifi_rssi_to_strength(int8_t rssi);
