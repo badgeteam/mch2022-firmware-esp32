8aa044a6d202a4cd8706a0b1d679af13564f028b
RobotMan2412 <julian@scheffers.net>
2022-06-08 19:16:29 +0200

    OPTIMISE some PNG shenanigans.

--- main/bootscreen.c
+++ main/bootscreen.c
@@ -13,10 +13,9 @@ void display_boot_screen(pax_buf_t* pax_buffer, ILI9341* ili9341, const char* te
     
     pax_noclip(pax_buffer);
     pax_background(pax_buffer, 0xFFFFFF);
-    pax_buf_t logo;
-    pax_decode_png_buf(&logo, (void*) mch2022_logo_png_start, mch2022_logo_png_end - mch2022_logo_png_start, PAX_BUF_16_565RGB, 0);
-    pax_draw_image(pax_buffer, &logo, (320 / 2) - (212 / 2), ((240 - 32 - 10) / 2) - (160 / 2));
-    pax_buf_destroy(&logo);
+    float x = (320 / 2) - (212 / 2);
+    float y = ((240 - 32 - 10) / 2) - (160 / 2);
+    pax_insert_png_buf(pax_buffer, mch2022_logo_png_start, mch2022_logo_png_end-mch2022_logo_png_start, x, y, 0);
     
     pax_vec1_t size = pax_text_size(font, 18, text);
     pax_draw_text(pax_buffer, 0xFF000000, font, 18, (320 / 2) - (size.x / 2), 240 - 32, text);
