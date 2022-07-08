2cfff064ec43413f4c1cc28ac9b8431f45040798
Renze Nicolai <renze@rnplus.nl>
2022-07-08 20:15:17 +0200

    Fix includes bump version to 0.9.1

--- main/include/app_management.h
+++ main/include/app_management.h
@@ -3,6 +3,8 @@
 #include <freertos/queue.h>
 #include <freertos/task.h>
 #include <stdbool.h>
+#include "ili9341.h"
+#include "pax_gfx.h"
 
 bool create_dir(const char* path);
 bool install_app(xQueueHandle button_queue, pax_buf_t* pax_buffer, ILI9341* ili9341, const char* type_slug, bool to_sd_card, char* data_app_info,
--- main/include/app_update.h
+++ main/include/app_update.h
@@ -5,5 +5,7 @@
 #include <sdkconfig.h>
 #include <stdio.h>
 #include <string.h>
+#include "ili9341.h"
+#include "pax_gfx.h"
 
 void update_apps(xQueueHandle button_queue, pax_buf_t* pax_buffer, ILI9341* ili9341);
